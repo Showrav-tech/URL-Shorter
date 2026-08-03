26c4b8534ab26539db4a0649f8b9d4956de73e55
Showrav <showravc85@gmail.com>
2026-08-03 14:07:08 +0600

    Replace repository with new project

---
+++ models/url.js
@@ -0,0 +1,19 @@
+const mongoose= require('mongose');
+
+const urlSchema=new mongoose.Schema(
+    {
+   shortId:{
+    type :String,
+    reqired:true,
+    unique:true,
+   },
+   redirectURL:{
+    type:String,
+    required:true,
+   },
+   visitHistory:[{timestamp:{type:Number}}],
+
+
+    },
+    {timestamps :true}
+);
