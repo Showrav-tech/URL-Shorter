bbc8e1fd05afa1b7f9b47b8ddda66b8c01fb12a0
Showrav <showravc85@gmail.com>
2026-08-04 12:05:33 +0600

    Add analytics routes

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const express = require("express");
 const {connectToMongoDB}=require("./connect");
 const urlRoute=require("./routes/url");
 const app=express();
+const URL=require('./models/url');
 const PORT =8000;
 const URL = require("./models/url");
 connectToMongoDB("mongodb://localhost:27017/short-url").then(
     console.log("Mongodb connected")
 );
+
+app.get('/:shortId',async (req,res)=>{
+ const shortId=req.params.shortId;
+ const entry= await URL.findOneAndUpdate(
+    {
+        shortId,
+    },
+    {
+       $push:{
+        visitHistory:{
+            timestamp:Date.now(),
+        },
+       } ,
+    }
+ );
+ res.redirect(entry.redirectURL);
+})
+
 app.use(express.json());
 app.use("/url",urlRoute);
 app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`));
